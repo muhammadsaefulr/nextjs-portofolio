b9a726f514aac3e49dc13b4d037cda399973c5ee
saepulid <saepull.id@gmail.com>
2024-08-10 15:28:52 +0700

    create: create repository

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/e-logo.png" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
   );
